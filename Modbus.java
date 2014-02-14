f08d6801c54242dae2c9e54b26c22f208d72c632
Michael Deakin <mfdeakin@cs.unc.edu>
2014-02-14 18:59:38 -0500

    Full TCP/Modbus protocol implemented for write, still needs testing

--- Modbus.java
+++ Modbus.java
@@ -10,6 +10,7 @@ import java.nio.ByteOrder;
 public class Modbus {
     private static final int MODBUS_PORT = 502;
     private static final byte MODBUS_READ = 0x03;
+    private static final byte MODBUS_WRITE = 0x10;
 
     public Modbus(String ip)
 	throws UnknownHostException
@@ -41,14 +42,9 @@ public class Modbus {
 	    System.err.print(errors[i]);
 	}
     }
-
-    public void readRegisters(short[] dest, short addr, short size)
-	throws IOException, Exception
+    
+    protected ByteBuffer createReadCmd(short addr, short size)
     {
-	/* Registers are either 2 or 4 bytes in size */
-	assert size % 2 == 0;
-	size /= 2;
-	Socket socket = new Socket(ip, MODBUS_PORT);
 	/* Tell the drive we need to read registers at the address
 	 * Modbus Read Command:
 	 * Transaction Identifier: Byte 1-2
@@ -65,20 +61,25 @@ public class Modbus {
 	cmd.putShort(tid);
 	tid++;
 	cmd.putShort((short)0);
-	cmd.putShort(readlen);
+	final short cmdlen = 6;
+	cmd.putShort(cmdlen);
 	cmd.put((byte)0);
 	cmd.put(MODBUS_READ);
 	cmd.putShort(addr);
 	cmd.putShort(size);
+	return cmd;
+    }
+    
+    public void readRegisters(short[] dest, short addr, short size)
+	throws IOException, Exception
+    {
+	/* Registers are either 2 or 4 bytes in size */
+	assert size % 2 == 0;
+	size /= 2;
+	Socket socket = new Socket(ip, MODBUS_PORT);
+	ByteBuffer cmd = createReadCmd(addr, size);
 	socket.getOutputStream().write(cmd.array());
-	/* Get the values of the registers.
-	 * First wait for it to have arrived
-	 * Then read the packet.
-	 * Response Format:
-	 * Function Code: Byte 0
-	 * Byte Count: Byte 1
-	 * Register Values: Bytes 2-(2*size+2)
-	 */
+	/* Block until the registers have all been received */
 	while(socket.getInputStream().available() < 2 + 2 * size &&
 	      socket.isConnected());
 	byte[] check = new byte[2];
@@ -101,22 +102,33 @@ public class Modbus {
 	socket.close();
     }
 
-    void writeRegisters(byte[] dest, short addr, short size)
+    void writeRegisters(short regs[], short addr)
 	throws IOException, Exception
     {
-	/* Registers are either 2 or 4 bytes in size */
-	assert size % 2 == 0;
-	size /= 2;
 	Socket socket = new Socket(ip, MODBUS_PORT);
 	/* Modbus Read Command:
-	 * Function Code: Byte 0
-	 * Starting Address: Bytes 1-2
-	 * Quantity of Registers: Bytes 3-4
+	 * Transaction Identifier: Byte 1-2
+	 * Protocol identifier = 0: Byte 3-4
+	 * Length (bytes) field (upper byte) = 0: Byte 5-6
+	 * Unit identifier (previously slave address): Byte 7
+	 * Function Code: Byte 8
+	 * Starting Address: Bytes 9-10
+	 * Quantity of Registers: Bytes 11-12
 	 */
-	ByteBuffer cmd = ByteBuffer.allocate(5);
-	cmd.put(MODBUS_READ);
+	ByteBuffer cmd = ByteBuffer.allocate(12 + 2 * regs.length);
+	cmd.putShort(tid);
+	tid++;
+	cmd.putShort((short)0);
+	short length = (short)(7 + 2 * regs.length);
+	cmd.putShort(length);
+	cmd.put((byte)0);
+	cmd.put(MODBUS_WRITE);
 	cmd.putShort(addr);
-	cmd.putShort(size);
+	cmd.putShort((short)regs.length);
+	cmd.put((byte)(regs.length * 2));
+	for(int i = 0; i < regs.length; i++) {
+	    cmd.putShort(regs[i]);
+	}
 	socket.getOutputStream().write(cmd.array());
     }
 
