17222711ce30af5d18a5a51854055a89a67388a6
mfdeakin <mdever44@gmail.com>
2014-02-18 15:17:38 -0500

    Added support for multiple slave devices

--- Modbus.java
+++ Modbus.java
@@ -1,5 +1,6 @@
 
 import java.io.IOException;
+import java.lang.Integer;
 import java.net.Socket;
 import java.net.InetAddress;
 import java.net.Inet4Address;
@@ -11,15 +12,24 @@ public class Modbus {
     private static final int MODBUS_PORT = 502;
     private static final byte MODBUS_READ = 0x03;
 
-    public Modbus(String ip)
+    public Modbus(String ip, byte slave)
 	throws UnknownHostException
     {
 	this.ip = Inet4Address.getByName(ip);
+	this.slave = slave;
     }
 
-    public Modbus(InetAddress ip)
+    public Modbus(byte[] ip, byte slave)
+	throws UnknownHostException
+    {
+	this.ip = Inet4Address.getByAddress(ip);
+	this.slave = slave;
+    }
+
+    public Modbus(InetAddress ip, byte slave)
     {
 	this.ip = ip;
+	this.slave = slave;
     }
 
     private void printException(byte[] response)
@@ -43,9 +53,6 @@ public class Modbus {
     public void readRegisters(short[] dest, short addr, short size)
 	throws IOException, Exception
     {
-	/* Registers are either 2 or 4 bytes in size */
-	assert size % 2 == 0;
-	size /= 2;
 	Socket socket = new Socket(ip, MODBUS_PORT);
 	/* Tell the drive we need to read registers at the address
 	 * Modbus Read Command:
@@ -53,12 +60,21 @@ public class Modbus {
 	 * Starting Address: Bytes 1-2
 	 * Quantity of Registers: Bytes 3-4
 	 */
-	ByteBuffer cmd = ByteBuffer.allocate(5);
+	ByteBuffer cmd = ByteBuffer.allocate(6);
 	cmd.order(ByteOrder.BIG_ENDIAN);
+	cmd.put(slave);
 	cmd.put(MODBUS_READ);
 	cmd.putShort(addr);
 	cmd.putShort(size);
+
+	for(int i = 0; i < 6; i++) {
+	    byte b = cmd.get(i);
+	    System.out.printf("0x%02x ", b);
+	}
+	System.out.print("\n");
+
 	socket.getOutputStream().write(cmd.array());
+
 	/* Get the values of the registers.
 	 * First wait for it to have arrived
 	 * Then read the packet.
@@ -67,26 +83,27 @@ public class Modbus {
 	 * Byte Count: Byte 1
 	 * Register Values: Bytes 2-(2*size+2)
 	 */
-	while(socket.getInputStream().available() < 2 + 2 * size &&
-	      socket.isConnected());
-	byte[] check = new byte[2];
-	socket.getInputStream().read(check);
-	/* Got the header, check for errors */
-	assert(check[0] == MODBUS_READ);
-	if(socket.getInputStream().available() < 2 + 2 * size) {
-	    printException(check);
-	    /* Because I'm a terrible and lazy programmer and it's midnight */
-	    throw new Exception();
-	}
-	/* We're clear. Now just get the data,
-	 * and put it in a usable format (easier said than done) */
-	byte[] temp = new byte[2 * size];
-	socket.getInputStream().read(temp);
-	ByteBuffer buffer = ByteBuffer.allocate(2 * size);
-	buffer.order(ByteOrder.BIG_ENDIAN);
-	buffer.put(temp);
-	for(int i = 0; i < size; i++)
-	    dest[i] = buffer.getShort();
+	// while(socket.getInputStream().available() < 9 + 2 * size &&
+	//       socket.isConnected())
+	//     System.out.printf("Received %d bytes\n", socket.getInputStream().available());
+	// byte[] check = new byte[2];
+	// socket.getInputStream().read(check);
+	// /* Got the header, check for errors */
+	// assert(check[0] == MODBUS_READ);
+	// if(socket.getInputStream().available() < 2 * size) {
+	//     printException(check);
+	//     /* Because I'm a terrible and lazy programmer and it's midnight */
+	//     throw new Exception();
+	// }
+	// /* We're clear. Now just get the data,
+	//  * and put it in a usable format (easier said than done) */
+	// byte[] temp = new byte[2 * size];
+	// socket.getInputStream().read(temp);
+	// ByteBuffer buffer = ByteBuffer.allocate(2 * size);
+	// buffer.order(ByteOrder.BIG_ENDIAN);
+	// buffer.put(temp);
+	// for(int i = 0; i < size; i++)
+	//     dest[i] = buffer.getShort();
 	socket.close();
     }
 
@@ -110,4 +127,5 @@ public class Modbus {
     }
 
     InetAddress ip;
+    byte slave;
 }
